1344a591a7d95d9a53cbf9f2a7a1e0249e3ba256
Andrew Moran <andrewmo@mit.edu>
2016-11-07 15:35:39 -0800

    Completed aiming overview

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -4,9 +4,9 @@
 #include "TankBarrel.h"
 
 
-void UTankBarrel::Elevate(float DegreesPerSecond)
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Barrel-Elevate() called"));
+	
 }
 
 

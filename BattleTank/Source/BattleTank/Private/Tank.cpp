1de9824c73e6af80b956f8048411f7f48fe8458c
Andrew Moran <andrewmo@mit.edu>
2016-11-13 13:31:07 -0800

    Made aiming work with updated ensure ops

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,16 +13,12 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s TEMP: Tank C++ Construct"), *TankName)
 }
 
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();	// Needed for BP to run
-
-	auto TankName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%s TEMP: Tank C++ Begin Play"), *TankName)
+	TankAimingComponent = FindComponentByClass<UTankAimingComponent>();
 }
 
 void ATank::AimAt(FVector HitLocation)
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -31,6 +31,7 @@ private:
 
 	virtual void BeginPlay() override;
 
+	//TODO remove after refactoring
 	UPROPERTY(EditDefaultsOnly, Category = "Firing")
 	float LaunchSpeed = 4000;
 

e585341417c095c05d6e3f7c975cfb3abdf5506b
Andrew Moran <andrewmo@mit.edu>
2016-11-14 00:51:46 -0800

    Added sideways friction

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,6 +3,23 @@
 #include "BattleTank.h"
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	// Acceleration spped at this frame
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	// Calculate and apply sideways (F = m a)
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
+
+	TankRoot->AddForce(CorrectionForce);
+}
+
 
 void UTankTrack::SetThrottle(float Throttle)
 {

1567d06018eef1aab82b8e892a95bd4b0d40b7e0
Andrew Moran <andrewmo@mit.edu>
2016-11-14 13:58:35 -0800

    Made aiming clamped to shortest dist

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -91,8 +91,16 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDir)
 	auto AimAsRotator = AimDir.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
+	// Always yaw shortest dist
 	Barrel->Elevate(DeltaRotator.Pitch);
-	Turret->Rotate(DeltaRotator.Yaw);
+	if (DeltaRotator.Yaw < 180)
+	{
+		Turret->Rotate(DeltaRotator.Yaw);
+	}
+	else {
+		Turret->Rotate(-DeltaRotator.Yaw);
+	}
+
 }
 
 void UTankAimingComponent::Fire()

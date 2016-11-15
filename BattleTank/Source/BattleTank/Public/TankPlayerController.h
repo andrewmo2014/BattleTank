a741f6426bcf42e6fd8ba0e612180240e5dd3668
Andrew Moran <andrewmo@mit.edu>
2016-11-15 02:16:09 -0800

    Player detaches on tank death

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -20,9 +20,14 @@ protected:
 	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
 	void FoundAimingComponent(UTankAimingComponent* AimCompRef);
 
+	UFUNCTION()
+	void OnPossessedTankDeath();
+
 private:
 
-	virtual void BeginPlay() override;	
+	virtual void SetPawn(APawn * InPawn) override;
+
+	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
 

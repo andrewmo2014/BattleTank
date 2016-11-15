793362d8a1a5bfb61cd622b482e4e80698e0a358
Andrew Moran <andrewmo@mit.edu>
2016-11-14 17:22:26 -0800

    Added particle effect

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -28,6 +28,7 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* CollisionMesh = nullptr;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
 	
 };
